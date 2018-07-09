0b9e2a5f0320b26b36cd963c4fdf7fde62931383
Jason Viger <jason.viger@gmail.com>
2018-07-09 10:59:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,53 @@
+var addElementToBeginningOfArray = function(array, element){
+  var newArray = array;
+  newArray = newArray.unshift(element);
+  return newArray;
+  
+}
+
+var destructivelyAddElementToBeginningOfArray = function(){
+   array.unshift(element);
+   return newArray;
+  
+}
+
+
+var addElementToEndOfArray = function(array){
+   var newArray = array.push(element);
+   return newArray;
+}
+var destructivelyAddElementToEndOfArray = function(){
+   array.push(element);
+   return array;
+}
+
+var accessElementInArray = function(array,index){
+  return array[index];
+  
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = function(array){
+  return array.slice(1);
+
+}
+
+
+var removeElementFromBeginningOfArray = function(array){
+  var newArray = array.slice(1);
+  return newArray;
+  
+}
+
+var destructivelyRemoveElementFromEndOfArray = function(array){
+  array = array.pop();
+  return array;
+}
+var removeElementFromEndOfArray = function(array){
+  var newArray = array.pop();
+  return newArray;
+}
+
+
+
+
+
